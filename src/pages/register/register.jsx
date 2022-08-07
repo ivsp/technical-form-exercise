a440d1e0cea8874f7a80e3a5138f12efd9e68600
ivsp <ivsp93@gmail.com>
2022-08-07 02:21:56 +0200

    Creo el formulario base con todos los inputs.

---
+++ src/pages/register/register.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import RegisterForm from "../../components/register-form/register-form";
+
+function Register() {
+  return (
+    <React.Fragment>
+      <RegisterForm></RegisterForm>
+    </React.Fragment>
+  );
+}
+
+export default Register;
