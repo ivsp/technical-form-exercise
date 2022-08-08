dc278869ccb730b2ec0642004a1efce89ea7dde4
ivsp <ivsp93@gmail.com>
2022-08-08 02:15:04 +0200

    Añado cometario explicando la forma de enviar el objeto FormData al back.

--- src/functions/api.functions.js
+++ src/functions/api.functions.js
@@ -3,6 +3,8 @@ export async function registerNewUser(body) {
   const r = await fetch(`http://localhost:4000/users/register`, {
     method: "POST",
     body: new URLSearchParams(body),
+    //Envío el FormData como un URL-enconded string.
+    //Fuente: https://openjavascript.info/2022/04/26/post-form-data-using-javascripts-fetch-api/
   });
 
   return r;
